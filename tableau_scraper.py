5a3ce224edd50a2c60b39cf3351b7ba87baec251
Camila Fernandes <108847180+CamilaFernandesdev@users.noreply.github.com>
2023-04-06 17:27:18 -0300

    Update tableau_scraper.py

--- tableau_scraper.py
+++ tableau_scraper.py
@@ -1,34 +1,56 @@
-
-"""
-Tableau geralmente usa uma técnica chamada "embbeding" para exibir suas visualizações.
-O que significa que as visualizações são exibidas em um iframe separado em vez de diretamente
-no código HTML da página.
-"""
-
 import time
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
 
 class TableauScraper:
-    
-    def __init__(self, modo= 'tradicional'| 'servidor'):
-        """Inicialize the Chrome."""
-        if 'tradicional':
-            self.driver = webdriver.Chrome()
-        elif 'servidor':
-            options = Options()
-            self.driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
-            time.sleep(20)
-        else:
-            print('Escolha uma das opções de modo: tradicional ou servidor')
-            
 
-    def accept_cookies(self, cookies_xpath: str):
+    def __init__(self, url):
+        self.URL = url
+        self.driver = self.open_chrome()
+        self.driver.get(self.URL)
+        time.sleep(4)
+        print('Entered website')
+
+
+    def _open_chrome(self):
+        """Inicialize the Chrome."""
+        options = webdriver.ChromeOptions()
+        options = ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-session-crashed-bubble")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-popup-blocking")
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-features=VizDisplayCompositor")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--disable-site-isolation-trials")
+        options.add_argument("--dns-prefetch-disable")
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--ignore-ssl-errors")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--disable-web-security")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-features=IsolateOrigins,site-per-process")
+        options.add_argument("--disable-browser-side-navigation")
+        options.add_argument("--disable-infobars")
+    
+        driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+        return driver
+
+    def accept_cookies(self, cookies_xpath:str):
         """Para aceitar os cookies.
-        
+                
         Parameters
         ----------
         cookies_xpath
@@ -53,71 +75,74 @@ class TableauScraper:
         try:
             cookie_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, cookies_xpath)))
             cookie_btn.click()
+            time.sleep(4)
+            print('Accepted cookies')
         except:
-            pass
+            print('No cookies banner')
 
-    def go_to_page(self, url: str, cookies_xpath: str):
-        self.driver.get(url)
-        time.sleep(4)
-        self.accept_cookies(cookies_xpath)
-        time.sleep(4)
-        self.driver.maximize_window()
-        time.sleep(4)
-        
-        
-    def switch_tableau(self):
-        self.driver.switch_to.frame(0)
-        time.sleep(4)
-
-    def find_elements_css(self,localizador: str):
-        try:
-            if modos=='xpath':
-                self.driver.find_element(By.XPATH, localizador)
-                time.sleep(40)
-            elif modos=='CSS':
-                self.driver.find_element(By.CSS_SELECTOR, localizador)
-                time.sleep(40)
-            else:
-                print('')
-        except:    
-            pass
-    def return_page(self):
-        self.driver.switch_to.default_content()
-        time.sleep(4)
-
-    def download_data(self):
-        self.switch_to_iframe(0)
-        painel_download_dados = self.driver.find_element(By.CSS_SELECTOR, ".tabLastPoint .tabScrollerContentWindow")
-        painel_download_dados.click()
-        time.sleep(30)
-        self.switch_to_default_content()
-        self.driver.execute_script("window.scrollBy(0, 800);")
-        self.switch_to_iframe(0)
-        icone_download = self.driver.find_element(By.CSS_SELECTOR, ".tab-icon-download")
-        icone_download.click()
-        time.sleep(1)
-        self.driver.find_element(By.CSS_SELECTOR, ".fdofgby:nth-child(4)").click()
-        time.sleep(1)
-        select_csv = self.driver.find_element(By.CSS_SELECTOR, ".f81g0pf:nth-child(2)")
-        select_csv.click()
-        time.sleep(1)
-        detalhamento_dados = self.driver.find_element(By.CSS_SELECTOR, ".f7ypqvd")
-        detalhamento_dados.click()
-        time.sleep(130)
-
-    def close(self):
-        self.driver.quit()
    
 
+    def _open_tableau(self):
+        self.driver.switch_to.frame(0)
+        time.sleep(4)
+        print('Switching to iframe Tableau')
+        
+    def select_tableau_dashboard(self, css_selector: str):
+        self._open_tableau()
+        elements = self.driver.find_element(By.CSS_SELECTOR, css_selector)
+        elements.click()
+        print('Selecting dashboard and waiting downloaded page')
+        time.sleep(30)
+        
+    def select_tableau_rodape(self):
+        self.driver.switch_to.default_content()
+        self.driver.execute_script("window.scrollBy(0, 800);")
+        time.sleep(4)
+        self._open_tableau()
+        
+    def find_element(self, modo: str, localizador: str):
+        modo = str().lower().split()
+        try:
+            if modo=='xpath':
+                self.driver.find_element(By.XPATH, localizador).click()
+                print('Selecting element in dashboard')
+                time.sleep(10)
+                
+            elif modo=='CSS':
+                self.driver.find_element(By.CSS_SELECTOR, localizador).click()
+                print('Selecting element in dashboard')
+                time.sleep(10)
+                
+            else:
+                print('Forneça um modo de busca: xpath ou css')
+                
+        except:    
+            print('Elements dont finded')
+        
+        
+    # def download_data(self):
+    #     self.driver.switch_to.default_content()
+    #     self.driver.execute_script("window.scrollBy(0, 800);")
+    #     self.timer()
+    #     self.driver.switch_to.frame(0)
+    #     self.timer()
+    #     print('Downloading data')
+    #     # icone_download = self.driver.find_element(By
+        
+    def close(self):
+        return self.driver.quit()
+        
+        
 if __name__ == "__main__":
     # Constantes 
     COOKIES_XPATH = "//div[@class='cookie-content d-flex p-3 justify-content-around align-items-center']//button"
     URL = 'https://www.ccee.org.br/web/guest/dados-e-analises/consumo'
+    COOKIES_XPATH = '//*[@id="onetrust-accept-btn-handler"]'
 
 
 
-def steps_download_consumo_tableau():
-    tableau_scrapper = TableauScrapper()
-    tableau_scrapper.go_to_consumo_page()
-    tableau_scrapper.download_data()
-    tableau_scrapper.close()
+    def steps_download_consumo_tableau():
+        tableau_scrapper = TableauScrapper()
+        tableau_scrapper.go_to_consumo_page()
+        tableau_scrapper.download_data()
+        tableau_scrapper.close()
